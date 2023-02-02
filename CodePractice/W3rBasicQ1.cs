8c0a501a29d2be8a7fd96371822b7cf990f43ff4
Sai Sreenivas Reddy Karra <42874557+sreenivas98@users.noreply.github.com>
2023-02-01 21:21:52 -0600

    Create W3rBasicQ1.cs

---
+++ CodePractice/W3rBasicQ1.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            Console.WriteLine("Sai Sreenivas Reddy Karra");
+        }
+    }
+}
