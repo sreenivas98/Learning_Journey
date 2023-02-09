ceed7ead0ea931375e0ebc2baf85221b41ae9aee
Sai Sreenivas Reddy Karra <42874557+sreenivas98@users.noreply.github.com>
2023-02-09 10:26:55 -0600

    Update ExplicitCasting.cs

--- w3schools_practice/ExplicitCasting.cs
+++ w3schools_practice/ExplicitCasting.cs
@@ -8,9 +8,12 @@ namespace MyApplication
     {
       double myDouble = 9.78;
       int myInt = (int) myDouble;  // Manual casting: double to int
-
+	    double myDouble2 = 10.89;
+      int myInt2 = Convert.ToInt32(myDouble2); // Manual Casting using built-in method: double to int
       Console.WriteLine(myDouble);
       Console.WriteLine(myInt);
+      Console.WriteLine(myDouble2);
+      Console.WriteLine(myInt2);
     }
   }
 }
