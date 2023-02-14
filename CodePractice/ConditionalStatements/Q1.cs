fc8204386e38750854db5877d5d869d9fd622a3a
Sai Sreenivas Reddy Karra <42874557+sreenivas98@users.noreply.github.com>
2023-02-14 10:03:28 -0600

    Create Q1.cs

---
+++ CodePractice/ConditionalStatements/Q1.cs
@@ -0,0 +1,11 @@
+using System;
+class W3rConditionalQ1{
+  static void Main() {
+    int x= Convert.ToInt32(Console.ReadLine());
+    int y= Convert.ToInt32(Console.ReadLine());
+    if(x==y)
+        Console.WriteLine("Equal");
+    else
+        Console.WriteLine("Not equal");
+  }
+}
