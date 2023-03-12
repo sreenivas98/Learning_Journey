f8b9e13494d4979d82366b544764d2725267bc30
Sai Sreenivas Reddy Karra <42874557+sreenivas98@users.noreply.github.com>
2023-03-12 13:09:45 -0500

    Create InheritanceEx1.cs

---
+++ CodePractice/OOP/InheritanceEx1.cs
@@ -0,0 +1,39 @@
+using System;
+
+// base class
+class Animal { 
+
+    public string name;
+
+    public void display() {
+      Console.WriteLine("I am an animal");
+    }
+    
+  } 
+  
+  // derived class of Animal 
+  class Dog : Animal {
+    
+    public void getName() {
+      Console.WriteLine("My name is " + name);
+    }
+  }
+
+  class InheritanceEx1 {
+
+    static void Main(string[] args) {
+
+      // object of derived class
+      Dog labrador = new Dog();
+
+      // access field and method of base class
+      labrador.name = "Buddy";
+      labrador.display();
+
+      // access method from own class
+      labrador.getName();
+
+      Console.ReadLine();
+    }
+
+  }
